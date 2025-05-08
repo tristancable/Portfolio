ab4ec37997193a17ee26f7a8b9d2ad10365f6d99
tristancable <tcable@student.neumont.edu>
2025-05-08 12:09:53 -0600

    Added main structure of website

---
+++ astro-portfolio/postcss.config.mjs
@@ -0,0 +1,9 @@
+import tailwindcss from '@tailwindcss/postcss7-compat';
+import autoprefixer from 'autoprefixer';
+
+export default {
+    plugins: [
+        tailwindcss,
+        autoprefixer,
+    ],
+}
